853b894203e9940ddeed264c84a4d2aa06d65fb6
poonambudde <buddepoonam123@gmail.com>
2022-02-11 06:56:53 +0530

    ADDED: Number is Prime or not

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            bool IsPrime = true;
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    IsPrime = false;
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                Console.WriteLine("Number is Prime.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Prime.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
