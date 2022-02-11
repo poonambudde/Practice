ad6041ab386ece4fad2aa3032d308e987a97d721
poonambudde <buddepoonam123@gmail.com>
2022-02-11 07:21:28 +0530

    ADDED: Even and Odd number between a range

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine("\nOdd Numbers :");
+            for (i = 1; i <= 30; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
